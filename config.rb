ad1adae75b3f8c80c5f0eedb1c8a1c2c83193ded
Nick Rutten <nick@sparrowinspace.com>
2015-06-14 14:54:15 +0200

    finish header and add  gem

--- config.rb
+++ config.rb
@@ -1,13 +1,41 @@
 require "extensions/views"
 
-activate :views
-activate :directory_indexes
-activate :protect_emails
+# SETTINGS
+# These are settings you should change.
+
+# Fill in your name here.
+set :name, "Nick Rutten"
+
+# Fill in your role here.
+set :role, "UI :: UX :: IxD :: Front-end"
+
+# Set up portfolio title
+activate :title, site: 'Nick Rutten', separator: ' :: '
 
 # Google Analytics tracking
 activate :google_analytics do |ga|
   ga.tracking_id = 'UA-64084727-1' # Replace with your property ID.
 end
+
+Time.zone = "Amsterdam"
+
+# END SETTINGS
+# Do not change anything beyond this line.
+
+# Enable multilanguage support
+activate :i18n
+
+# Enable and configure portfolio items
+activate :blog do |blog|
+  # Blog options
+  blog.sources   = "portfolio/{category}/{title}.html"
+  blog.permalink = "portfolio/{category}/{title}.html"
+end
+
+activate :views
+activate :directory_indexes
+activate :protect_emails
+activate :syntax
 
 set :relative_links, true
 set :css_dir, 'assets/stylesheets'
@@ -15,6 +43,7 @@ set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
 set :fonts_dir, 'assets/fonts'
 set :layout, 'layouts/application'
+set :partials_dir, 'layouts/partials'
 
 configure :development do
  activate :livereload
--- source/assets/javascripts/application.js.coffee
+++ source/assets/javascripts/application.js.coffee
@@ -1,1 +1,18 @@
 //= require_tree .
+
+$(document).ready ->
+  menuToggle = $('#js-mobile-menu').unbind()
+  $('#js-navigation-menu').removeClass 'show'
+  menuToggle.on 'click', (e) ->
+    e.preventDefault()
+    $('#js-navigation-menu').slideToggle ->
+      if $('#js-navigation-menu').is(':hidden')
+        $('#js-navigation-menu').removeAttr 'style'
+      return
+    return
+  return
+
+Webfont.load ->
+  google: {
+    families: ['Roboto', 'Roboto Mono']
+  }
