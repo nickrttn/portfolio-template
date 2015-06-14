22f62bdb6276011f9b13d22d020c40a99cb6e25b
Nick Rutten <nick@sparrowinspace.com>
2015-06-14 14:54:46 +0200

    Automated commit at 2015-06-14 12:54:46 UTC by middleman-deploy 1.0.0

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,4 +1,24 @@
 (function() {
+  $(document).ready(function() {
+    var menuToggle;
+    menuToggle = $('#js-mobile-menu').unbind();
+    $('#js-navigation-menu').removeClass('show');
+    menuToggle.on('click', function(e) {
+      e.preventDefault();
+      $('#js-navigation-menu').slideToggle(function() {
+        if ($('#js-navigation-menu').is(':hidden')) {
+          $('#js-navigation-menu').removeAttr('style');
+        }
+      });
+    });
+  });
 
+  Webfont.load(function() {
+    return {
+      google: {
+        families: ['Roboto', 'Roboto Mono']
+      }
+    };
+  });
 
 }).call(this);
